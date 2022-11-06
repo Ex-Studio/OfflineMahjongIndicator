3e6516bcd7f5fd3ee0d4da5d2f003550ec5cb6de
yxj <564197835@qq.com>
2022-11-07 06:51:16 +0800

    add states

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,11 +20,52 @@ class ContextView extends StatelessWidget {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Row 1'),
-          Text('Row 2'),
-        ],
+        children: [Text('Row 1'), Text('Row 2'), MyView()],
       ),
+    );
+  }
+}
+
+class MyView extends StatefulWidget {
+  const MyView({super.key});
+
+  @override
+  State<MyView> createState() => _MyViewState();
+}
+
+class _MyViewState extends State<MyView> {
+  int counterJu = 0;
+  int counterChang = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("东$counterJu局"),
+        Text("$counterChang本场"),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  debugPrint("pressed");
+                  setState(() {
+                    counterJu += 1;
+                  });
+                },
+                child: Text("下一场")),
+            ElevatedButton(
+                onPressed: () {
+                  debugPrint("pressed");
+                  setState(() {
+                    counterChang += 1;
+                  });
+                },
+                child: Text("加一本场")),
+          ],
+        ),
+      ],
     );
   }
 }
