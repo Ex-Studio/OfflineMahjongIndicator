1e019313aa1aa5f0558b7893345e34c24ed358d3
yxj <564197835@qq.com>
2022-11-08 02:45:56 +0800

    add north...

--- lib/main.dart
+++ lib/main.dart
@@ -23,27 +23,37 @@ class ContextView extends StatelessWidget {
 ${YxjExtensions.screenWidth(context)}x${YxjExtensions.screenHeight(context)}
 """;
 
+    String usageInfo = """
+[使用说明]
+将手机朝东家横向放置
+长按解锁按键进行操作
+""";
+
     return Stack(
       children: [
         YxjCornerWidget(
             padding: 40.0, corner: YxjCorner.topLeft, child: Text(debugInfo)),
+        YxjCornerWidget(
+            padding: 40.0,
+            corner: YxjCorner.bottomLeft,
+            child: Text(usageInfo)),
         YxjCenterView(),
         YxjCornerWidget(
             corner: YxjCorner.bottomLeft,
             rotationQuarterTurns: 0,
-            child: Text("东1局" + " " + "𝛩" * 2)),
+            child: Text("東1局" + " " + "𝛩" * 2)),
         YxjCornerWidget(
             corner: YxjCorner.bottomRight,
             rotationQuarterTurns: 1,
-            child: Text("东1局" + " " + "𝛩" * 2)),
+            child: Text("東1局" + " " + "𝛩" * 2)),
         YxjCornerWidget(
             corner: YxjCorner.topRight,
             rotationQuarterTurns: 2,
-            child: Text("东1局" + " " + "𝛩" * 2)),
+            child: Text("東1局" + " " + "𝛩" * 2)),
         YxjCornerWidget(
             corner: YxjCorner.topLeft,
             rotationQuarterTurns: 3,
-            child: Text("东1局" + " " + "𝛩" * 2)),
+            child: Text("東1局" + " " + "𝛩" * 2)),
       ],
     );
   }
@@ -57,23 +67,38 @@ class YxjCenterView extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Row(
+        Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                debugPrint("pressed");
-              },
-              style: ElevatedButton.styleFrom(
-                shape: CircleBorder(),
-                padding: EdgeInsets.all(16),
-                backgroundColor: Colors.blue, // <-- Button color
-                foregroundColor: Colors.white, // <-- Splash color
-              ),
-              child: Icon(
-                CupertinoIcons.lock,
-                size: YxjExtensions.screenLengthShorter(context) * 0.1,
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [RotatedBox(quarterTurns: 2, child: Text("西"))],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RotatedBox(quarterTurns: 1, child: Text("北")),
+                ElevatedButton(
+                  onPressed: () {
+                    debugPrint("pressed");
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(16),
+                    backgroundColor: Colors.blue, // <-- Button color
+                    foregroundColor: Colors.white, // <-- Splash color
+                  ),
+                  child: Icon(
+                    CupertinoIcons.lock,
+                    size: YxjExtensions.screenLengthShorter(context) * 0.1,
+                  ),
+                ),
+                RotatedBox(quarterTurns: 3, child: Text("南")),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [RotatedBox(quarterTurns: 0, child: Text("東"))],
             ),
           ],
         ),
